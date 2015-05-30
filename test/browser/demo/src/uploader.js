5680ccd898e12b96d05b872f1d280c3165dd3628
leeight <leeight@gmail.com>
2015-05-30 23:52:07 +0800

    重构了一下上传文件的功能，整理的 helper.upload 接口可以让其它的功能模块来使用

--- test/browser/demo/src/uploader.js
+++ test/browser/demo/src/uploader.js
@@ -9,6 +9,7 @@ define(function (require) {
     var async = require('async');
 
     var Klient = require('./client');
+    var helper = require('./helper');
 
     var exports = {};
 
@@ -18,177 +19,49 @@ define(function (require) {
             return false;
         }
 
-        var file = files[0];
-        var partSize = parseInt($('#g_part_size').val(), 10) * 1024 * 1024;
+        $('#g_time').val('0s');
+        var startTime = new Date().getTime();
+        async.eachLimit(files, 1, upload, function (err) {
+            if (!err) {
+                var endTime = new Date().getTime();
+                $('#g_time').html(((endTime - startTime) / 1000) + 's');
+            }
+            else {
+                console.error(err);
+            }
+        });
+    }
+
+    function upload(file, callback) {
+        var partSize = 5 * 1024 * 1024;
         var chunkCount = Math.ceil(file.size * 1.0 / partSize);
 
         $('#g_file_size').val(file.size);
         $('#g_chunk_count').val(chunkCount);
         $('#g_progress').val(0);
-        $('#g_time').val('0s');
         $('#g_url').html('-');
 
-        if (file.size <= partSize) {
-            uploadSingleFile2(file);
-        }
-        else {
-            uploadSuperFile(file);
-        }
-    }
-
-    function getTasks(file, uploadId) {
-        var leftSize = file.size;
-        var minPartSize = 5 * 1024 * 1024;
-        var offset = 0;
-        var partNumber = 1;
-
-        var tasks = [];
-
-        while (leftSize > 0) {
-            var partSize = Math.min(leftSize, minPartSize);
-
-            tasks.push({
-                file: file,
-                uploadId: uploadId,
-                partNumber: partNumber,
-                partSize: partSize,
-                start: offset,
-                stop: offset + partSize - 1
-            });
-
-            leftSize -= partSize;
-            offset += partSize;
-            partNumber += 1;
-        }
-
-        return tasks;
-    }
-
-    function uploadPartFile(state) {
-        return function (item, callback) {
-            // item.file
-            // item.uploadId
-            // item.start
-            // item.stop
-            // item.partNumber
-            // item.partSize
-            var blob = item.file.slice(item.start, item.stop + 1);
-            var client = Klient.createInstance();
-            var key = item.file.name;
-            var bucket = $('#g_bucket').val();
-
-            client.uploadPartFromBlob(bucket, key, item.uploadId, item.partNumber, item.partSize, blob)
-                .then(function (res) {
-                    $('#g_progress').val((++state.completed) / state.totalCount);
-                    callback(null, res);
-                })
-                .catch(function (err) {
-                    callback(err);
-                });
-        };
-    }
-
-    function uploadSuperFile(file) {
-        var startTime = new Date().getTime();
-
         var client = Klient.createInstance();
         var key = file.name;
-        var bucket_name = $('#g_bucket').val();
+        var bucketName = $('#g_bucket').val();
 
-        var ext = key.split(/\./g).pop();
-
-        // Firefox在POST的时候，Content-Type 一定会有Charset的，因此
-        // 这里不管3721，都加上.
-        var mimeType = sdk.MimeType.guess(ext) + '; charset=UTF-8';
-        var options = {
-            'Content-Type': mimeType
-        };
-
-        var uploadId = null;
-        client.initiateMultipartUpload(bucket_name, key, options)
-            .then(function (response) {
-                uploadId = response.body.uploadId;
-
-                var deferred = sdk.Q.defer();
-                var tasks = getTasks(file, uploadId);
-                var state = {
-                    completed: 0,
-                    totalCount: tasks.length
-                };
-                var parallel = parseInt($('#g_parallel').val(), 10);
-                async.mapLimit(tasks, parallel, uploadPartFile(state), function (err, results) {
-                    if (err) {
-                        deferred.reject(err);
-                    }
-                    else {
-                        deferred.resolve(results);
-                    }
-                });
-                return deferred.promise;
-            })
-            .then(function (all_response) {
-                var part_list = [];
-                all_response.forEach(function (response, index) {
-                    part_list.push({
-                        partNumber: index + 1,
-                        eTag: response.http_headers.etag
-                    });
-                });
-
-                return client.completeMultipartUpload(bucket_name, key, uploadId, part_list);
-            })
-            .then(function () {
-                $('#g_progress').val(1);
-                var url = client.generatePresignedUrl(bucket_name, key);
-                $('#g_url').html('<a href="' + url + '" target="_blank">下载地址</a>');
-            })
-            .catch(function (error) {
-                console.error(error);
-                $('#g_progress').val(1);
-            })
-            .fin(function () {
-                var endTime = new Date().getTime();
-                $('#g_time').html(((endTime - startTime) / 1000) + 's');
-            });
-    }
-
-    function uploadSingleFile2(file, opt_startByte, opt_stopByte) {
-        var startTime = new Date().getTime();
-
-        var client = Klient.createInstance();
-        var key = file.name;
-        var ext = key.split(/\./g).pop();
-        var bucket = $('#g_bucket').val();
-
-        var start = opt_startByte || 0;
-        var stop = opt_stopByte || (file.size - 1);
-        var blob = file.slice(start, stop + 1);
-
-        var mimeType = sdk.MimeType.guess(ext);
-        if (/^text\//.test(mimeType)) {
-            mimeType += '; charset=UTF-8';
-        }
-        var options = {
-            'Content-Type': mimeType
-        };
-        client.on('progress', function (evt) {
+        client.on('bosprogress', function (evt) {
             if (evt.lengthComputable) {
                 $('#g_progress').val(evt.loaded / evt.total);
             }
         });
-        return client.putObjectFromBlob(bucket, key, blob, options)
-            .then(function (res) {
-                console.log(res);
+        helper.upload(bucketName, key, file, null, client)
+            .then(function () {
                 $('#g_progress').val(1);
-                var url = client.generatePresignedUrl(bucket, key);
+                var url = client.generatePresignedUrl(bucketName, key);
                 $('#g_url').html('<a href="' + url + '" target="_blank">下载地址</a>');
             })
-            .catch(function (err) {
-                console.error(err);
+            .catch(function (error) {
+                $('#g_progress').val(1);
+                callback(error);
             })
             .fin(function () {
-                var endTime = new Date().getTime();
-                $('#g_time').html(((endTime - startTime) / 1000) + 's');
+                callback(null);
             });
     }
 
@@ -223,6 +96,9 @@ define(function (require) {
 
     exports.init = function () {
         $('#file').on('change', handleFileSelect);
+        $('#upload').on('click', function (e) {
+            $('#file').click();
+        });
         $('input[type=radio]').on('click', switchMode);
         $('legend.collapse').on('click', toggleLegend);
         $('#g_host').val(location.protocol + '//' + location.host);
