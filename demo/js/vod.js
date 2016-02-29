97931868c4b6d53a18cd24857d1631a2db7d278c
zhouhua <zhouhua@baidu.com>
2016-02-29 17:50:53 +0800

    修改vod demo

--- demo/js/vod.js
+++ demo/js/vod.js
@@ -29,6 +29,7 @@ $(document).ready(function () {
                     html += '<tr data-id="' + media.mediaId + '">' +
                         '<td>' + media.attributes.title + '</td>' +
                         '<td>' + media.attributes.description + '</td>' +
+                        '<td class="media-id">' + media.mediaId + '</td>' +
                         '<td>' +
                         '<a href="javascript:void 0;" class="btn link-btn delete">删除</a>' +
                         '<a href="javascript:void 0;" class="btn link-btn url">获取视频地址</a>' +
@@ -42,14 +43,41 @@ $(document).ready(function () {
         });
     }
 
-    updateList();
+    // updateList();
     $('#upload').on('change', function (evt) {
         var file = evt.target.files[0];
-        vodClient.createMediaResource(file.name, '测试文件', file).then(function () {
-            toastr.success('上传成功');
-            updateList();
+        vodClient = new sdk.VodClient(vodConfig, bosConfig);
+        var $row = $('<tr class="uploading">' +
+            '<td>' + file.name + '</td>' +
+            '<td>测试文件</td>' +
+            '<td class="media-id">' +
+            '<div class="progress"><div class="progress-bar progress-bar-success" style="width: 0;"></div></div>' +
+            '</td>' +
+            '<td>' +
+            '<a href="javascript:void 0;" class="btn link-btn delete">删除</a>' +
+            '<a href="javascript:void 0;" class="btn link-btn url">获取视频地址</a>' +
+            '<a href="javascript:void 0;" class="btn link-btn code">获取播放代码</a>' +
+            '</td></tr>');
+        vodClient.createMediaResource(file.name, '测试文件', file).then(function (res) {
+            // 可以通过res.body.mediaId获取上传完成的媒体资源id
+            var id = res.body.mediaId;
+            toastr.success('上传成功, 媒体资源id为: ' + id);
+            $row.removeClass('uploading').attr('data-id', id).find('.media-id').text(id);
         }).catch(function (err) {
             toastr.error('上传失败');
+        });
+        $fileList.append($row);
+        vodClient.on('progress', function (evt) {
+            // 处理上传进度
+            if (evt.uploadId) {
+                // 如果是分块上传,各个分块上传时也会发出progress事件,为了简化代码,先忽略这类事件
+                return;
+            }
+            if (evt.lengthComputable) {
+                var width = (evt.loaded / evt.total) * 100;
+                $row.find('.progress-bar').css('width', width + '%')
+                    .text(width.toFixed(2) + '%');
+            }
         });
     });
 
@@ -57,7 +85,8 @@ $(document).ready(function () {
         var mediaId = $(e.target).closest('tr').attr('data-id');
         vodClient.deleteMediaResource(mediaId).then(function () {
             toastr.success('删除成功');
-            updateList();
+            // updateList();
+            $(e.target).closest('tr').remove();
         }).catch(function (err) {
             toastr.error('删除失败');
         });
