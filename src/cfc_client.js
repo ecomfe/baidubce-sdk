6ad3e7b70b2c072c2e7e75061a868ce13170bb7c
sunlei <lei.sunstar@gmail.com>
2018-02-27 14:58:34 +0800

    fecs

--- src/cfc_client.js
+++ src/cfc_client.js
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
  *
- * Licensed under the Apache License, Version 2.0 (the 'License'); you may not use this file except in compliance with
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
  * @file src/cfc_client.js
@@ -16,7 +16,7 @@
 
 /* eslint-env node */
 /* eslint max-params:[0,10] */
-/* eslint fecs-camelcase:[2,{'ignore':['/opt_/']}] */
+/* eslint fecs-camelcase:[2,{"ignore":["/opt_/"]}] */
 
 var util = require('util');
 
